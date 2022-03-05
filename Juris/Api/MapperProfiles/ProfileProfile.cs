b7c50bf2a8c09ee70b43c4d88f3f54d80bf1213c
Marcel Vlasenco <marcelvlasenco@gmail.com>
2022-03-05 19:12:40 +0200

    Add AutoMapper profiles

---
+++ Juris/Api/MapperProfiles/ProfileProfile.cs
@@ -0,0 +1,13 @@
+﻿using Juris.Api.Dtos.Profile;
+using Juris.Models.Entities;
+
+namespace Juris.Api.MapperProfiles;
+
+public class ProfileProfile : AutoMapper.Profile
+{
+    public ProfileProfile()
+    {
+        CreateMap<Profile, ProfileDto>();
+        CreateMap<Profile, UpdateProfileDto>().ReverseMap();
+    }
+}
