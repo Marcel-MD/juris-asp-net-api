7c68a17a7500220f36f95021d3ed04474beb14fd
Marcel Vlasenco <marcelvlasenco@gmail.com>
2022-04-25 11:19:39 +0300

    Add review controller tests

---
+++ Juris.Test/Integration/TestBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Juris.Api.Dtos.User;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Juris.Test.Integration;
+
+public class TestBase
+{
+    protected readonly HttpClient _client;
+
+    protected TestBase()
+    {
+        var appFactory = new WebApplicationFactory<Program>();
+        _client = appFactory.CreateClient();
+    }
+
+    protected async Task Authenticate(string email, string password)
+    {
+        var response = await _client.PostAsJsonAsync("api/user/login", new CreateUserDto()
+        {
+            Email = email,
+            Password = password
+        });
+
+        if (!response.IsSuccessStatusCode)
+            throw new Exception($"Can't authenticate user: {email} {password}");
+
+        var dto = await response.Content.ReadFromJsonAsync<UserTokenDto>();
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", dto.Token);
+    }
+}
